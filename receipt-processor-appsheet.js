7077f4a78401145ce6dc8d7ab6ee453b0239fbfe
HDM-Juan <administracion@hospitaldelmovil.com.mx>
2024-10-07 18:52:20 -0600

    Update receipt-processor-appsheet.js

--- receipt-processor-appsheet.js
+++ receipt-processor-appsheet.js
@@ -1,19 +1,29 @@
-// Al inicio del archivo receipt.js, añada esta función:
-function obtenerDatosDeURL() {
-    const params = new URLSearchParams(window.location.search);
-    const datosJSON = params.get('datos');
-    if (datosJSON) {
-        return JSON.parse(decodeURIComponent(datosJSON));
-    }
-    return null;
+<script>
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
-// Modifique la función window.onload así:
 window.onload = function() {
-    const datos = obtenerDatosDeURL();
-    if (datos) {
-        actualizarRecibo(datos);
-    } else {
-        console.error('No se encontraron datos en la URL');
+    var datos = getUrlParameter('datos').split('<<>>');
+    var variables = [
+        'NOMBRE_EMPRESA', 'DIRECCION', 'EMAIL', 'TELEFONO', 'SITIO_WEB', 'WHATSAPP',
+        'LOGO_URL', 'PUBLICIDAD_URL', 'FOLIO_VENTA', 'FECHA_VENTA', 'FOLIO_RECEPCION',
+        'IMEI', 'NO_SERIE', 'DETALLE_VENTAS', 'TOTAL_VENTA', 'PAGOS', 'TOTAL_PAGOS',
+        'TEXTO_GARANTIA', 'OBSERVACIONES', 'REDES_SOCIALES_URL'
+    ];
+    
+    for (var i = 0; i < variables.length; i++) {
+        var elements = document.getElementsByClassName(variables[i]);
+        for (var j = 0; j < elements.length; j++) {
+            if (elements[j].tagName === 'IMG') {
+                elements[j].src = datos[i*2 + 1];
+            } else {
+                elements[j].innerHTML = datos[i*2 + 1];
+            }
+        }
     }
-};
+}
+</script>
