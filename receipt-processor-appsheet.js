02f1e0554945994f0c46d3ef5f83230358172ea5
HDM-Juan <administracion@hospitaldelmovil.com.mx>
2024-10-08 21:48:37 -0600

    Update receipt-processor-appsheet.js

--- receipt-processor-appsheet.js
+++ receipt-processor-appsheet.js
@@ -1,4 +1,3 @@
-<script>
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
@@ -6,24 +5,62 @@ function getUrlParameter(name) {
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
-window.onload = function() {
-    var datos = getUrlParameter('datos').split('<<>>');
-    var variables = [
-        'NOMBRE_EMPRESA', 'DIRECCION', 'EMAIL', 'TELEFONO', 'SITIO_WEB', 'WHATSAPP',
-        'LOGO_URL', 'PUBLICIDAD_URL', 'FOLIO_VENTA', 'FECHA_VENTA', 'FOLIO_RECEPCION',
-        'IMEI', 'NO_SERIE', 'DETALLE_VENTAS', 'TOTAL_VENTA', 'PAGOS', 'TOTAL_PAGOS',
-        'TEXTO_GARANTIA', 'OBSERVACIONES', 'REDES_SOCIALES_URL'
-    ];
-    
-    for (var i = 0; i < variables.length; i++) {
-        var elements = document.getElementsByClassName(variables[i]);
-        for (var j = 0; j < elements.length; j++) {
-            if (elements[j].tagName === 'IMG') {
-                elements[j].src = datos[i*2 + 1];
-            } else {
-                elements[j].innerHTML = datos[i*2 + 1];
-            }
-        }
+function setElementText(id, text) {
+    var element = document.getElementById(id);
+    if (element) {
+        element.textContent = text;
     }
 }
-</script>
+
+function setElementSrc(id, src) {
+    var element = document.getElementById(id);
+    if (element) {
+        element.src = src;
+    }
+}
+
+window.onload = function() {
+    var datos = getUrlParameter('datos').split('<<>>');
+    var datosObj = {};
+
+    for (var i = 0; i < datos.length; i += 2) {
+        datosObj[datos[i].replace(/^<<|>>$/g, '')] = datos[i + 1];
+    }
+
+    setElementText('nombreEmpresa', datosObj.NOMBRE_EMPRESA);
+    setElementText('direccion', datosObj.DIRECCION);
+    setElementText('email', datosObj.EMAIL);
+    setElementText('telefono', datosObj.TELEFONO);
+    setElementText('sitioWeb', datosObj.SITIO_WEB);
+    setElementText('whatsapp', datosObj.WHATSAPP);
+    setElementSrc('logoUrl', datosObj.LOGO_URL);
+    setElementSrc('publicidadUrl', datosObj.PUBLICIDAD_URL);
+    setElementText('folioVenta', datosObj.FOLIO_VENTA);
+    setElementText('fechaVenta', datosObj.FECHA_VENTA);
+    setElementText('folioRecepcion', datosObj.FOLIO_RECEPCION);
+    setElementText('imei', datosObj.IMEI);
+    setElementText('noSerie', datosObj.NO_SERIE);
+    setElementText('totalVenta', datosObj.TOTAL_VENTA);
+    setElementText('totalPagos', datosObj.TOTAL_PAGOS);
+    setElementText('textoGarantia', datosObj.TEXTO_GARANTIA);
+    setElementText('observaciones', datosObj.OBSERVACIONES);
+    setElementSrc('redesSocialesUrl', datosObj.REDES_SOCIALES_URL);
+
+    // Procesar DETALLE_VENTAS
+    var detalleVentas = JSON.parse(datosObj.DETALLE_VENTAS);
+    var tbodyDetalleVentas = document.querySelector('#detalleVentas tbody');
+    detalleVentas.forEach(function(detalle) {
+        var row = tbodyDetalleVentas.insertRow();
+        row.insertCell(0).textContent = detalle.concepto;
+        row.insertCell(1).textContent = detalle.precio;
+    });
+
+    // Procesar PAGOS
+    var pagos = JSON.parse(datosObj.PAGOS);
+    var tbodyPagos = document.querySelector('#pagos tbody');
+    pagos.forEach(function(pago) {
+        var row = tbodyPagos.insertRow();
+        row.insertCell(0).textContent = pago.cantidad;
+        row.insertCell(1).textContent = pago.formaPago;
+    });
+};
