cf0cc17040038d1883829d3f17657f0f8404744c
HDM-Juan <administracion@hospitaldelmovil.com.mx>
2024-10-09 06:48:55 -0600

    Update receipt-processor-appsheet.js

--- receipt-processor-appsheet.js
+++ receipt-processor-appsheet.js
@@ -73,36 +73,4 @@ window.onload = function() {
     setElementText('telefono', datosObj.TELEFONO);
     setElementText('sitioWeb', datosObj.SITIO_WEB);
     setElementText('whatsapp', datosObj.WHATSAPP);
-    setElementSrc('logoUrl', datosObj.LOGO_URL);
-    setElementSrc('publicidadUrl', datosObj.PUBLICIDAD_URL);
-    setElementText('folioVenta', datosObj.FOLIO_VENTA);
-    setElementText('fechaVenta', datosObj.FECHA_VENTA);
-    setElementText('folioRecepcion', datosObj.FOLIO_RECEPCION);
-    setElementText('imei', datosObj.IMEI);
-    setElementText('noSerie', datosObj.NO_SERIE);
-    setElementText('totalVenta', datosObj.TOTAL_VENTA);
-    setElementText('totalPagos', datosObj.TOTAL_PAGOS);
-    setElementText('textoGarantia', datosObj.TEXTO_GARANTIA);
-    setElementText('observaciones', datosObj.OBSERVACIONES);
-    setElementSrc('redesSocialesUrl', datosObj.REDES_SOCIALES_URL);
-
-    // Process DETALLE_VENTAS
-    console.log("Contenido de DETALLE_VENTAS:", datosObj.DETALLE_VENTAS);
-    var detalleVentasHTML = procesarSelect(datosObj.DETALLE_VENTAS);
-    console.log("HTML de Detalle Ventas procesado:", detalleVentasHTML);
-    if (detalleVentasHTML) {
-        setElementHTML('detalleVentasBody', detalleVentasHTML);
-    } else {
-        setElementHTML('detalleVentasBody', '<tr><td colspan="2">No hay detalles de venta disponibles</td></tr>');
-    }
-
-    // Process PAGOS
-    console.log("Contenido de PAGOS:", datosObj.PAGOS);
-    var pagosHTML = procesarSelect(datosObj.PAGOS);
-    console.log("HTML de Pagos procesado:", pagosHTML);
-    if (pagosHTML) {
-        setElementHTML('pagosBody', pagosHTML);
-    } else {
-        setElementHTML('pagosBody', '<tr><td colspan="2">No hay información de pagos disponible</td></tr>');
-    }
-};
+    setElementSrc('logo
