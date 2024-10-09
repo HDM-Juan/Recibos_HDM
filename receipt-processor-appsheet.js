2c1e16e1aef07e4a43c6eded371cde0c368f08c3
HDM-Juan <administracion@hospitaldelmovil.com.mx>
2024-10-09 00:03:26 -0600

    Update receipt-processor-appsheet.js

--- receipt-processor-appsheet.js
+++ receipt-processor-appsheet.js
@@ -8,37 +8,73 @@ function getUrlParameter(name) {
 function setElementText(id, text) {
     var element = document.getElementById(id);
     if (element) {
-        element.textContent = text;
+        element.textContent = text || '';
     }
 }
 
 function setElementSrc(id, src) {
     var element = document.getElementById(id);
     if (element) {
-        element.src = src;
+        element.src = src || '';
     }
+}
+
+function setElementHTML(id, html) {
+    var element = document.getElementById(id);
+    if (element) {
+        element.innerHTML = html || '';
+    }
+}
+
+function procesarSelect(contenido) {
+    var regex = /<<Start: SELECT\((.*?)\) >>([\s\S]*?)<< end >>/g;
+    var resultado = contenido;
+    var match;
+
+    while ((match = regex.exec(contenido)) !== null) {
+        var selectContent = match[2];
+        // Aquí puedes procesar el contenido del SELECT si es necesario
+        resultado = resultado.replace(match[0], selectContent);
+    }
+
+    return resultado;
 }
 
 window.onload = function() {
     var datosRaw = getUrlParameter('datos');
-    console.log("Datos crudos recibidos:", datosRaw);
-
-    // Mostrar datos crudos en la página
-    var debugElement = document.createElement('pre');
-    debugElement.textContent = "Datos crudos recibidos:\n" + datosRaw;
-    document.body.insertBefore(debugElement, document.body.firstChild);
-
     var datos = datosRaw.split('<<>>');
-    console.log("Datos separados:", datos);
-
     var datosObj = {};
+
     for (var i = 0; i < datos.length; i += 2) {
         var key = datos[i].replace(/^<<|>>$/g, '');
         var value = datos[i + 1];
         datosObj[key] = value;
-        console.log(key + ": " + value);
     }
 
-    // Resto del código...
-    // (Mantén el código existente que establece los valores en el HTML)
+    setElementText('nombreEmpresa', datosObj.NOMBRE_EMPRESA);
+    setElementText('direccion', datosObj.DIRECCION);
+    setElementText('email', datosObj.EMAIL);
+    setElementText('telefono', datosObj.TELEFONO);
+    setElementText('sitioWeb', datosObj.SITIO_WEB);
+    setElementText('whatsapp', datosObj.WHATSAPP);
+    setElementSrc('logoUrl', datosObj.LOGO_URL);
+    setElementSrc('publicidadUrl', datosObj.PUBLICIDAD_URL);
+    setElementText('folioVenta', datosObj.FOLIO_VENTA);
+    setElementText('fechaVenta', datosObj.FECHA_VENTA);
+    setElementText('folioRecepcion', datosObj.FOLIO_RECEPCION);
+    setElementText('imei', datosObj.IMEI);
+    setElementText('noSerie', datosObj.NO_SERIE);
+    setElementText('totalVenta', datosObj.TOTAL_VENTA);
+    setElementText('totalPagos', datosObj.TOTAL_PAGOS);
+    setElementText('textoGarantia', datosObj.TEXTO_GARANTIA);
+    setElementText('observaciones', datosObj.OBSERVACIONES);
+    setElementSrc('redesSocialesUrl', datosObj.REDES_SOCIALES_URL);
+
+    // Procesar DETALLE_VENTAS
+    var detalleVentasHTML = procesarSelect(datosObj.DETALLE_VENTAS);
+    setElementHTML('detalleVentasBody', detalleVentasHTML);
+
+    // Procesar PAGOS
+    var pagosHTML = procesarSelect(datosObj.PAGOS);
+    setElementHTML('pagosBody', pagosHTML);
 };
