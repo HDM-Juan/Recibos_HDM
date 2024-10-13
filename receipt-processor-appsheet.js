e6edd5850508ed997ec37217db74e2158a32a4a1
HDM-Juan <administracion@hospitaldelmovil.com.mx>
2024-10-12 18:41:02 -0600

    Update receipt-processor-appsheet.js

--- receipt-processor-appsheet.js
+++ receipt-processor-appsheet.js
@@ -1,82 +1,70 @@
 function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
 function setElementText(id, text) {
-    var element = document.getElementById(id);
-    if (element) {
-        element.textContent = text || '';
-    }
+  var element = document.getElementById(id);
+  if (element) {
+    element.textContent = text || '';
+  }
 }
 
 function setElementSrc(id, src) {
-    var element = document.getElementById(id);
-    if (element) {
-        element.src = src || '';
-    }
+  var element = document.getElementById(id);
+  if (element) {
+    element.src = src || '';
+  }
 }
 
-function setElementHTML(id, html) {
-    var element = document.getElementById(id);
-    if (element) {
-        element.innerHTML = html || '';
-    }
-}
-
-function procesarSelect(contenido) {
-    console.log("Procesando SELECT:", contenido);
-    var regex = /<<Start: SELECT\((.*?)\) >>([\s\S]*?)<< end >>/;
-    var match = regex.exec(contenido);
-    
-    if (match) {
-        console.log("Contenido dentro de SELECT:", match[2]);
-        return match[2];
-    }
-    console.log("No se encontró un patrón SELECT válido");
-    return '';
+function generarTablaHTML(datos, columnas) {
+  // Usamos un array para construir el HTML y luego lo unimos, 
+  // esto es más eficiente que concatenar cadenas en cada iteración.
+  var html = ['<table><thead><tr>'];
+  columnas.forEach(function(columna) {
+    html.push('<th>' + columna + '</th>');
+  });
+  html.push('</tr></thead><tbody>');
+  datos.forEach(function(fila) {
+    html.push('<tr>');
+    columnas.forEach(function(columna) {
+      // Accedemos a las propiedades del objeto 'fila' usando la notación de corchetes
+      // para que funcione con nombres de columna que tengan espacios.
+      html.push('<td>' + fila[columna] + '</td>'); 
+    });
+    html.push('</tr>');
+  });
+  html.push('</tbody></table>');
+  return html.join(''); // Unimos el array en una sola cadena
 }
 
 window.onload = function() {
-    var datosRaw = getUrlParameter('datos');
-    console.log("Datos crudos recibidos:", datosRaw);
+  var datosRaw = getUrlParameter('datos');
+  console.log("Datos crudos recibidos:", datosRaw);
 
-    var datos = datosRaw.split('<<>>');
-    console.log("Datos separados:", datos);
+  try {
+    var datos = JSON.parse(datosRaw);
+    console.log("Datos JSON:", datos);
 
-    var datosObj = {};
-    for (var i = 0; i < datos.length; i += 2) {
-        var key = datos[i].replace(/^<<|>>$/g, '');
-        var value = datos[i + 1];
-        datosObj[key] = value;
-        console.log(key + ": " + value);
-    }
-
-    // Establecer valores en el HTML
-    for (var key in datosObj) {
-        if (datosObj.hasOwnProperty(key)) {
-            if (key !== 'DETALLE_VENTAS' && key !== 'PAGOS') {
-                var element = document.getElementById(key);
-                if (element) {
-                    if (element.tagName === 'IMG') {
-                        element.src = datosObj[key];
-                    } else {
-                        element.textContent = datosObj[key];
-                    }
-                }
-            }
+    // Iteramos sobre las propiedades del objeto JSON
+    for (var key in datos) {
+      if (datos.hasOwnProperty(key)) { 
+        if (key.endsWith('_URL')) { // Si la clave termina en "_URL", establecemos el atributo src
+          setElementSrc(key, datos[key]);
+        } else if (key === 'DETALLE_VENTAS') {
+          var detalleVentasHTML = generarTablaHTML(datos[key], ['Concepto', 'Precio Unitario']);
+          setElementHTML('detalleVentasBody', detalleVentasHTML);
+        } else if (key === 'PAGOS') {
+          var pagosHTML = generarTablaHTML(datos[key], ['Cantidad', 'Forma de Pago']);
+          setElementHTML('pagosBody', pagosHTML);
+        } else { // En otro caso, establecemos el contenido de texto
+          setElementText(key, datos[key]);
         }
+      }
     }
-
-    // Procesar DETALLE_VENTAS
-    var detalleVentasHTML = procesarSelect(datosObj.DETALLE_VENTAS);
-    console.log("HTML de Detalle Ventas:", detalleVentasHTML);
-    setElementHTML('detalleVentasBody', detalleVentasHTML);
-
-    // Procesar PAGOS
-    var pagosHTML = procesarSelect(datosObj.PAGOS);
-    console.log("HTML de Pagos:", pagosHTML);
-    setElementHTML('pagosBody', pagosHTML);
+  } catch (error) {
+    console.error("Error al procesar los datos JSON:", error);
+  }
 };
