a4ace45a22e6b6ae5ffd3d80089761b0f447cb15
HDM-Juan <administracion@hospitaldelmovil.com.mx>
2024-10-12 18:21:35 -0600

    Update receipt-processor-appsheet.js

--- receipt-processor-appsheet.js
+++ receipt-processor-appsheet.js
@@ -27,37 +27,44 @@ function setElementHTML(id, html) {
 }
 
 function procesarSelect(contenido) {
-    var regex = /<<Start: SELECT\((.*?)\) >>([\s\S]*?)<< end >>/g;
-    var resultado = contenido;
-    var match;
-
-    while ((match = regex.exec(contenido)) !== null) {
-        resultado = resultado.replace(match[0], match[2]);
+    console.log("Procesando SELECT:", contenido);
+    var regex = /<<Start: SELECT\((.*?)\) >>([\s\S]*?)<< end >>/;
+    var match = regex.exec(contenido);
+    
+    if (match) {
+        console.log("Contenido dentro de SELECT:", match[2]);
+        return match[2];
     }
-
-    return resultado.replace(/<<\[(.*?)\]>>/g, '');
+    console.log("No se encontró un patrón SELECT válido");
+    return '';
 }
 
 window.onload = function() {
     var datosRaw = getUrlParameter('datos');
-    var datos = datosRaw.split('<<>>');
-    var datosObj = {};
+    console.log("Datos crudos recibidos:", datosRaw);
 
+    var datos = datosRaw.split('<<>>');
+    console.log("Datos separados:", datos);
+
+    var datosObj = {};
     for (var i = 0; i < datos.length; i += 2) {
         var key = datos[i].replace(/^<<|>>$/g, '');
         var value = datos[i + 1];
         datosObj[key] = value;
+        console.log(key + ": " + value);
     }
 
     // Establecer valores en el HTML
     for (var key in datosObj) {
         if (datosObj.hasOwnProperty(key)) {
-            var element = document.getElementById(key);
-            if (element) {
-                if (element.tagName === 'IMG') {
-                    element.src = datosObj[key];
-                } else {
-                    element.textContent = datosObj[key];
+            if (key !== 'DETALLE_VENTAS' && key !== 'PAGOS') {
+                var element = document.getElementById(key);
+                if (element) {
+                    if (element.tagName === 'IMG') {
+                        element.src = datosObj[key];
+                    } else {
+                        element.textContent = datosObj[key];
+                    }
                 }
             }
         }
@@ -65,9 +72,11 @@ window.onload = function() {
 
     // Procesar DETALLE_VENTAS
     var detalleVentasHTML = procesarSelect(datosObj.DETALLE_VENTAS);
+    console.log("HTML de Detalle Ventas:", detalleVentasHTML);
     setElementHTML('detalleVentasBody', detalleVentasHTML);
 
     // Procesar PAGOS
     var pagosHTML = procesarSelect(datosObj.PAGOS);
+    console.log("HTML de Pagos:", pagosHTML);
     setElementHTML('pagosBody', pagosHTML);
 };
